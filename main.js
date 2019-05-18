1e61bae3db5033f9d631d36630d1897136948978
Rexben <rexben.rb@gmail.com>
2019-05-18 12:54:34 +0100

    functions and array methods

--- main.js
+++ main.js
@@ -132,3 +132,130 @@ speak();
 speak();
 speak();
 speak();
+
+
+//conditional statements
+
+const string2 = '2';
+const num2 = 5;
+
+console.log(string2 == num2); //true
+console.log(string2 === num2); //false
+
+if (num2 === 2)
+{
+  console.log('correct');
+} else
+{
+  console.log('wrong');
+}
+
+const roundOff = 5;
+if (roundOff === 4.986)
+{
+  console.log('correct');
+} else if (roundOff === 5)
+{
+  console.log('correct');
+} else if (roundOff === 4.99)
+{
+  console.log('correct');
+} else
+{
+  console.log('wrong');
+}
+
+//modulus %
+const mod = 5 % 2;
+if (mod === 0)
+{
+  console.log('2 is a factor of 5')
+} else
+{
+  console.log('2 is not a factor of 5')
+}
+
+//functions with parameters
+
+function add(x, y) {
+  return `The addition of ${x} and ${y} = ${x + y}`;
+}
+
+console.log(add(2,3));
+
+function welcomeUsers(name) {
+  return `Welcome, ${name} to Teencode camp`;
+}
+
+console.log(welcomeUsers('Lola'));
+
+const checkFactorial = (number, factor) => {
+  if (number % factor === 0 && typeof(number) === 'number')
+  {
+    return `${factor} is a factor of ${number}`
+  } else if (typeof(number) !== 'number' || typeof(factor) !== 'number')
+  {
+    return `Pls, input a number`
+  } else
+  {
+    return `${factor} is not a factor of ${number}`
+
+  }
+}
+
+console.log(checkFactorial('25', 5));
+
+//function expression
+const hello = function (name) {
+  return `Hello ${name}`;
+}
+
+console.log(hello('Rex'));
+
+//Arrow functions
+const hi = (name) => {
+  const val = 'Hello';
+  return `${val} ${name}`
+};
+
+const hi2 = name => `Hello ${name}`;
+
+// const forEach = (callback) => {
+//   const array = [0, 1, 3];
+//   callback(array);
+// }
+
+console.log(hi('John'));
+
+const arrofAges = [26, 56, 78, 9, 20, 44, 16, 10];
+
+//for each
+
+// const callback = function (age) {
+//   console.log(`Your age is ${age}`);
+// }
+
+//for in loop
+for (const age in arrofAges)
+{
+  console.log(arrofAges[age]);
+}
+
+arrofAges.forEach(age => console.log(`Your age is ${age}`));
+
+//map
+// const agesAfter5years = arrofAges.map(age => age + 5);
+// console.log(agesAfter5years);
+
+//chaining
+arrofAges.map(age => age + 5).forEach(age => console.log(age));
+
+const greaterThan18 = arrofAges.filter(age => age > 18);
+
+console.log(greaterThan18);
+
+const ages = [1, 2, 3, 4];
+
+const sumOfAges = ages.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sumOfAges);
